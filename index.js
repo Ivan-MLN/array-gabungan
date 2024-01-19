4f647e819f7e68d20700dc6e04f8516c114f014f
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-01-19 18:42:44 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require ("express")
+const { send } = require('micro');
+const { createServer } = require('http');
+const express = require('express');
 
-const app = express ()
-app.use(express.static('.'))
-app.get("/", (req, res) => {
-  res.sendFile(process.cwd() + "/index.html")
-})
-app.listen("3000", () => console.log("Running"))
+const app = express();
+app.use(express.static('.'));
+
+app.get('/', (req, res) => {
+  send(res, 200, 'Hello from Express.js on Vercel!');
+});
+
+const server = createServer(app);
+
+module.exports = server;
