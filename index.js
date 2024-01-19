9d52d602a5e2a59570a16777820fd8ed351b9cf1
Ivanzz <78632369+Ivan-MLN@users.noreply.github.com>
2024-01-19 18:15:13 +0700

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ app.use(express.static('.'))
 app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/index.html")
 })
-app.listen("5000", () => console.log("Running"))
+app.listen("8080", () => console.log("Running"))
